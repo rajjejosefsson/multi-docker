22efbd1efc3b22c86b3cc56a59a05a4acece1dfb
Rasmus Josefsson <rajosefs@microsoft.com>
2020-11-18 09:58:59 +0100

    update title

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import OtherPage from './OtherPage';
-import Fib from './Fib';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
+          <h1 style={{ color: "white", textAlign: "center" }}>
+            Fib Calculator
+          </h1>
           <img src={logo} className="App-logo" alt="logo" />
           <a
             className="App-link"
@@ -17,7 +20,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 3
+            Learn React
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
